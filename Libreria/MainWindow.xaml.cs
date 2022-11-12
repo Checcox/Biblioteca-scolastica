f3e90888d0a80ba795034294a88091dcd34530fb
Francesco Serrani <FRANCESCO.SERRANI@local.ittcesena.it>
2022-11-12 09:40:24 +0100

    aggiunto bottone per biblioteca

--- Libreria/MainWindow.xaml.cs
+++ Libreria/MainWindow.xaml.cs
@@ -28,12 +28,18 @@ namespace Libreria
 
         private void btnLibro_Click(object sender, RoutedEventArgs e)
         {
-             new Libro(txtTitolo.Text, txtAutore.Text,Int16.Parse(txtNumPagine.Text));         
+             new Libro(txtTitolo.Text, txtAutore.Text,Int16.Parse(txtNumPagine.Text));   
+            
         }
         
         private void btnIndiceLista_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void btnBiblioteca_Click(object sender, RoutedEventArgs e)
+        {
+            Biblioteca biblioteca = new Biblioteca(txtNomeB.Text, txtIndirizzo.Text, Convert.ToDouble(txtOrarioapertura.Text), Convert.ToDouble(txtorariochiusura.Text));
         }
     }
 }
